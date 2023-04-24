c7cf61c8b2d6fbcd577f8ccacbd3eafe69e85c0f
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-04-24 17:44:59 +0200

    Create basic PackingList entity

---
+++ PackIT.Domain/Entities/PackingList.cs
@@ -0,0 +1,10 @@
+﻿using PackIT.Domain.ValueObjects;
+
+namespace PackIT.Domain.Entity;
+
+public class PackingList
+{
+	public Guid Id { get; private set; }
+	private readonly PackingListName _name;
+	public readonly Localization _localization;
+}
