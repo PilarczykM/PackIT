801dd845efda827d008b4e31254a15596a822559
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-04-24 17:44:39 +0200

    Create value objects

---
+++ PackIT.Domain/ValueObjects/PackingName.cs
@@ -0,0 +1,20 @@
+﻿using PackIT.Domain.Exceptions;
+
+namespace PackIT.Domain.ValueObjects;
+
+public record PackingListName
+{
+    public string Value { get; }
+    public PackingListName(string value)
+    {
+        if(string.IsNullOrWhiteSpace(value))
+        {
+            throw new EmptyPackingListNameException();
+        }
+
+        Value = value;
+    }
+
+    public static implicit operator PackingListName(string value) => new(value);
+    public static implicit operator string(PackingListName value) => value.Value;
+}
