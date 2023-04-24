09822e073b32f82a7dce4e64af62f71e4b5c0020
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-04-24 17:49:23 +0200

    Constructor consumption

--- PackIT.Domain/Entities/PackingList.cs
+++ PackIT.Domain/Entities/PackingList.cs
@@ -7,4 +7,11 @@ public class PackingList
 	public Guid Id { get; private set; }
 	private readonly PackingListName _name;
 	public readonly Localization _localization;
+
+	internal PackingList(Guid id, PackingListName name, Localization localization)
+	{
+		Id = id;
+		_name = name;
+		_localization = localization;
+	}
 }
