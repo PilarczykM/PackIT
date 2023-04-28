daf51943f4585ef1ee8feb6cb1b22992371b3c37
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-04-28 17:53:28 +0200

    Prepare interface and not implemented factory

---
+++ PackIT.Domain/Factories/PackingListFactory.cs
@@ -0,0 +1,25 @@
+﻿using PackIT.Domain.Consts;
+using PackIT.Domain.Entity;
+using PackIT.Domain.ValueObjects;
+
+namespace PackIT.Domain.Factories
+{
+    public class PackingListFactory : IPackingListFactory
+    {
+        public PackingListFactory()
+        {
+        }
+
+        public PackingList Create(PackingListId id, PackingListName name, Localization localization)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PackingList CreateWithDefaultItems(PackingListId id, PackingListName name, TravelDays days,
+            Gender gender, Temperature temperature, Localization localization)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
