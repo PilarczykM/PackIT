4c11988df25aaca8463ce56b4e8b033167b17217
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-04-28 19:37:20 +0200

    Add policies for 'gender', 'temperature', 'universal'

---
+++ PackIT.Domain/Policies/Universal/BasicPolicy.cs
@@ -0,0 +1,22 @@
+﻿using PackIT.Domain.ValueObjects;
+
+namespace PackIT.Domain.Policies.Universal
+{
+    public class BasicPolicy : IPackingItemsPolicy
+    {
+        private const uint MaximumQuantity = 7;
+        public IEnumerable<PackingItem> GenerateItems(PolicyData policyData)
+            => new List<PackingItem>
+            {
+                new("Pants", Math.Min(policyData.Days, MaximumQuantity)),
+                new("Socks", Math.Min(policyData.Days, MaximumQuantity)),
+                new("T-Shirt", Math.Min(policyData.Days, MaximumQuantity)),
+                new("Trousers", Math.Min(policyData.Days, MaximumQuantity)),
+                new("Shampoo", 1),
+            };
+
+        public bool IsApplicable(PolicyData _)
+            => true;
+    }
+}
+
