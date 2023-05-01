5770bfa24644d91a7b38eb964e6f6b4ca0532bbe
PilarczykM <marcinpilarczyk@me.com>
2023-05-01 17:00:00 +0200

    Add implementation for infrastructure services and repositories

---
+++ PackIT.Infrastructure/EF/Services/PostgresPackingListReadService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PackIT.Application.Services;
+using PackIT.Infrastructure.EF.Contexts;
+using PackIT.Infrastructure.EF.Models;
+
+namespace PackIT.Infrastructure.EF.Services
+{
+    internal sealed class PostgresPackingReadService : IPackingListReadService
+    {
+        private readonly DbSet<PackingListReadModel> _packingLists;
+
+        public PostgresPackingReadService(ReadDbContext context)
+        {
+            _packingLists = context.PackingLists;
+        }
+
+        public Task<bool> ExistsByNameAsync(string name)
+            => _packingLists.AnyAsync(pl => pl.Name == name);
+    }
+}
+
