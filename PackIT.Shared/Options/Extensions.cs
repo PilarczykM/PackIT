4166b11fc47e699d273858bd2b0febb653bbe9f7
PilarczykM <marcinpilarczyk@me.com>
2023-05-01 15:48:09 +0200

    Add connection string into postgres option

---
+++ PackIT.Shared/Options/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace PackIT.Shared.Options
+{
+    public static class Extensions
+    {
+        public static TOptions GetOptions<TOptions>(this IConfiguration confuguration, string sectionName)
+            where TOptions : new()
+        {
+            var options = new TOptions();
+            confuguration.GetSection(sectionName).Bind(options);
+
+            return options;
+        }
+    }
+}
+
