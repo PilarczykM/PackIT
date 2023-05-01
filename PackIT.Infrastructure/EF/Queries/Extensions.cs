c66f19b95e2a7914dfb0514a42a7fc6de978eecf
PilarczykM <marcinpilarczyk@me.com>
2023-05-01 16:33:22 +0200

    Add implementation of query handlers

---
+++ PackIT.Infrastructure/EF/Queries/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using PackIT.Application.DTO;
+using PackIT.Infrastructure.EF.Models;
+
+namespace PackIT.Infrastructure.EF.Queries
+{
+    internal static class Extensions
+    {
+        public static PackingListDto AsDto(this PackingListReadModel readModel)
+        {
+            return new()
+            {
+                Id = readModel.Id,
+                Name = readModel.Name,
+                Localization = new LocalizationDto
+                {
+                    City = readModel.Localization.City,
+                    Country = readModel.Localization.Country
+                },
+                Items = readModel.Items?.Select(pi => new PackingItemDto
+                {
+                    Name = pi.Name,
+                    Quentity = pi.Quentity,
+                    IsPacked = pi.IsPacked
+                })
+            };
+        }
+    }
+}
+
---
+++ PackIT.Infrastructure/EF/Queries/Handlers/SeachPackingListsHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PackIT.Application.DTO;
+using PackIT.Application.Queries;
+using PackIT.Infrastructure.EF.Contexts;
+using PackIT.Infrastructure.EF.Models;
+using PackIT.Shared.Abstractions.Queries;
+
+namespace PackIT.Infrastructure.EF.Queries.Handlers
+{
+    internal class SeachPackingListsHandler : IQueryHandler<SeachPackingLists, IEnumerable<PackingListDto>>
+    {
+        private readonly DbSet<PackingListReadModel> _packingList;
+
+        public SeachPackingListsHandler(ReadDbContext context)
+        {
+            _packingList = context.PackingLists;
+        }
+
+        public async Task<IEnumerable<PackingListDto>> HandleAsync(SeachPackingLists query)
+        {
+            var dbQuery = _packingList
+                .Include(pl => pl.Items)
+                .AsQueryable();
+
+            if (query.SearchTerm is not null)
+            {
+                dbQuery
+                    .Where(pl =>
+                    Microsoft.EntityFrameworkCore.EF.Functions.ILike(pl.Name, $"%{query.SearchTerm}%"));
+            }
+
+            return await dbQuery
+                .Select(pl => pl.AsDto())
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
+
