2614cf2479bfdc0cc61396983753e10fdf6fae9e
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-05-01 12:20:19 +0200

    Add two queries without implementation

---
+++ PackIT.Application/Queries/SeachPackingLists.cs
@@ -0,0 +1,11 @@
+﻿using PackIT.Application.DTO;
+using PackIT.Shared.Abstractions.Queries;
+
+namespace PackIT.Application.Queries
+{
+    public class SeachPackingLists : IQuery<IEnumerable<PackingListDto>>
+    {
+        public string? SearchTerm { get; set; }
+    }
+}
+
