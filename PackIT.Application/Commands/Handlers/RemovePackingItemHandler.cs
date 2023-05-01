dae4175b0929f85a0e7fcaebef925fce783d55e4
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-05-01 10:35:09 +0200

    Prepare RemovePackingItem command with handler

---
+++ PackIT.Application/Commands/Handlers/RemovePackingItemHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PackIT.Application.Exceptions;
+using PackIT.Domain.Repositories;
+using PackIT.Shared.Abstractions.Commands;
+
+namespace PackIT.Application.Commands.Handlers
+{
+    public class RemovePackingItemHandler : ICommandHandler<RemovePackingItem>
+    {
+        private readonly IPackingListRepository _packingListRepository;
+        public RemovePackingItemHandler(IPackingListRepository packingListRepository)
+        {
+            _packingListRepository = packingListRepository;
+        }
+
+        public async Task HandleAsync(RemovePackingItem command)
+        {
+            var (packingListId, itemName) = command;
+
+            var packingList = await _packingListRepository.GetAsync(packingListId);
+
+            if (packingList is null)
+            {
+                throw new PackingListNotFoundException(packingListId);
+            }
+
+            packingList.RemoveItem(itemName);
+
+            await _packingListRepository.UpdateAsync(packingList);
+        }
+    }
+}
+
---
+++ PackIT.Application/Commands/RemovePackingItem.cs
@@ -0,0 +1,7 @@
+﻿using PackIT.Shared.Abstractions.Commands;
+
+namespace PackIT.Application.Commands
+{
+    public record RemovePackingItem(Guid PackingListId, string ItemName) : ICommand;
+}
+
