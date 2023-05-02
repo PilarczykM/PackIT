dfb47c2379c00e2998e8c2d2045f126983f891c8
PilarczykM <marcinpilarczyk@me.com>
2023-05-02 07:39:08 +0200

    Finish presentation layer as controllers. Fix singletonscope for ItemsPolicy. Correct Quantity table column name

--- PackIT.Infrastructure/EF/Models/PackingItemReadModel.cs
+++ PackIT.Infrastructure/EF/Models/PackingItemReadModel.cs
@@ -4,7 +4,7 @@
     {
         public Guid? Id { get; set; }
         public string Name { get; set; }
-        public uint Quentity { get; set; }
+        public uint Quantity { get; set; }
         public bool IsPacked { get; set; }
 
         public PackingListReadModel PackingList { get; set; }
