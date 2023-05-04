82fa2eb70d8d1e48ef2b73486b77a0f1e60dac48
PilarczykM <marcinpilarczyk@me.com>
2023-05-04 11:15:04 +0200

    Add error handling middleware

---
+++ PackIT.Shared/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using PackIT.Shared.Abstractions.Exceptions;
+
+namespace PackIT.Shared.Exceptions
+{
+    internal sealed class ExceptionMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await (next(context));
+            }
+            catch (PackItException e)
+            {
+                context.Response.StatusCode = 400;
+                context.Response.Headers.Add("content-type", "application/json");
+
+                var errorCode = ToUnderscoreCase(e.GetType().Name.Replace("Exception", ""));
+
+                var json = JsonSerializer.Serialize(new { ErrorCode = errorCode, e.Message });
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+
+        private static string ToUnderscoreCase(string value)
+            => string.Concat((value ?? string.Empty).Select((x, i) => i > 0 && char.IsUpper(x) && !char.IsUpper(value[i - 1]) ? $"_{x}" : x.ToString())).ToLower();
+    }
+}
+
